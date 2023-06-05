46aa8a6cc8140949799af2a021ad9fc1c4bd28d8
VukZh <vukfx@inbox.ru>
2023-06-05 20:12:38 +0600

    change Product not found code

--- src/handlers/getProductsById.ts
+++ src/handlers/getProductsById.ts
@@ -6,16 +6,20 @@ import { APIGatewayProxyEventPathParameters } from "aws-lambda";
 const getProduct = (pathParameters: APIGatewayProxyEventPathParameters) => {
   return PRODUCT.find((product) => product.id == pathParameters.productId);
 };
-export const handler = async (event: { pathParameters: { productId: string } }) => {
+export const handler = async (event: {
+  pathParameters: { productId: string };
+}) => {
   try {
-    const pathParameters = event.pathParameters || {}
+    const pathParameters = event.pathParameters || {};
     const product = getProduct(pathParameters);
     if (product) {
       return buildResponse(200, {
         product: product as ProductType,
       });
     }
-    throw ('Product not found');
+    return buildResponse(204, {
+      message: "Product not found",
+    });
   } catch (error) {
     return buildResponse(500, {
       message: error as string,
