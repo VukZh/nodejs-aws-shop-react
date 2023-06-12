86592b54734767a196afad0598ae18b2398aa63d
VukZh <vukfx@inbox.ru>
2023-06-13 00:15:24 +0600

    change path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://t00mw90bag.execute-api.eu-west-1.amazonaws.com",
-  order: "https://t00mw90bag.execute-api.eu-west-1.amazonaws.com",
-  import: "https://t00mw90bag.execute-api.eu-west-1.amazonaws.com",
-  bff: "https://t00mw90bag.execute-api.eu-west-1.amazonaws.com",
-  cart: "https://t00mw90bag.execute-api.eu-west-1.amazonaws.com",
+  product: "https://b5grmfd5a9.execute-api.eu-west-1.amazonaws.com",
+  order: "https://b5grmfd5a9.execute-api.eu-west-1.amazonaws.com",
+  import: "https://b5grmfd5a9.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://b5grmfd5a9.execute-api.eu-west-1.amazonaws.com",
+  cart: "https://b5grmfd5a9.execute-api.eu-west-1.amazonaws.com",
 };
 
 export default API_PATHS;
