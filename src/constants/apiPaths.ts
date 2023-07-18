da6ff90e996cbde9179c6c5c871e7723058b544d
VukZh <vukfx@inbox.ru>
2023-07-18 23:48:29 +0600

    refactoring for task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://zfysciwcoa.execute-api.eu-west-1.amazonaws.com",
-  order: "https://od25ml0dg5.execute-api.eu-west-1.amazonaws.com/api/profile",
+  order: "http://vukzh-cart-api-dev.eu-west-1.elasticbeanstalk.com/api/profile",
   import: "https://yqim7ns7ll.execute-api.eu-west-1.amazonaws.com",
   bff: "https://zfysciwcoa.execute-api.eu-west-1.amazonaws.com",
-  cart: "http://vukzh-cart-api-dev2.eu-west-1.elasticbeanstalk.com/api",
+  cart: "http://vukzh-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
