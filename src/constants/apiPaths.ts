44394e0bdb09af7ccda37f6928b978fcd1618508
VukZh <vukfx@inbox.ru>
2023-07-25 00:09:26 +0600

    change paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://zfysciwcoa.execute-api.eu-west-1.amazonaws.com",
   order: "https://od25ml0dg5.execute-api.eu-west-1.amazonaws.com/api/profile",
-  import: "https://q3tg485fq2.execute-api.eu-west-1.amazonaws.com",
-  bff: "https://zfysciwcoa.execute-api.eu-west-1.amazonaws.com",
+  import: "https://yqim7ns7ll.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://q3tg485fq2.execute-api.eu-west-1.amazonaws.com",
   cart: "https://od25ml0dg5.execute-api.eu-west-1.amazonaws.com/api",
 };
 
